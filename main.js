5d1082858117aae4db93ee38ee817aa078c4716a
JasurJunior <zaripovjasurbek2000@mail.ru>
2021-12-02 18:56:38 +0500

    hozir renderni tugatdim :)

--- main.js
+++ main.js
@@ -7,10 +7,70 @@ const URLS = [
 
   `https://api.themoviedb.org/3/movie/upcoming?api_key=${API_KEY}&page=`,
 ]
+let rangePAGES = 2
+let NOWpages
+const MAINdiv = document.querySelector('.append')
 
 START()
 
-async function START(URLpar = 0, PAGESpar = 1) {
-  let data = await fetch(URLS[URLpar])
-  console.log(data)
+async function START(_url = 0, _pages = 1) {
+  if (!(_pages <= rangePAGES)) return alert('range ERROR!')
+
+  let data = await fetch(URLS[_url] + _pages)
+  data = await data.json()
+  rangePAGES = data?.total_pages
+  renderFUN(data?.results)
 }
+
+function renderFUN(_data) {
+  MAINdiv.innerHTML = null
+  NOWpages = _data
+
+  for (const i of _data) {
+    MAINdiv.append(createFUN(i.title, i.vote_average, i.release_date, i.poster_path))
+  }
+}
+
+function createFUN(_name, _reyting, _date, _imgURL) {
+  let divMOVE = abs('div'),
+    IMG = abs('img'),
+    divINFO = abs('div'),
+    H3 = abs('h3'),
+    spanORANGE = abs('span'),
+    spanDATE = abs('span')
+
+  //class
+  divMOVE.className = 'movie'
+  divINFO.className = 'movie-info'
+  spanORANGE.className = 'orange'
+  spanDATE.className = 'date'
+
+  //text
+  H3.innerText = _name
+  spanORANGE.innerText = _reyting
+  spanDATE.innerText = _date
+
+  //img
+  IMG.setAttribute('src', 'https://image.tmdb.org/t/p/w500' + _imgURL)
+
+  //append
+  divINFO.append(H3, spanORANGE)
+  divMOVE.append(IMG, divINFO, spanDATE)
+
+  return divMOVE
+}
+
+function abs(htmlEL) {
+  return document.createElement(htmlEL)
+}
+/*
+          <!-- <div class="movie">
+                <img src="https://image.tmdb.org/t/p/w500/qRyy2UmjC5ur9bDi3kpNNRCc5nc.jpg" alt="Fast &amp; Furious Presents: Hobbs &amp; Shaw">
+   
+               <div class="movie-info">
+                   <h3>Fast &amp; Furious Presents: Hobbs &amp; Shaw</h3>
+                   <span class="orange">6.9</span>
+                </div>
+                <span class="date">2021-09-21</span>
+            </div> -->
+*/
