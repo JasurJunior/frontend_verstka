fd27630514f4c81129e904e3a9fdde3a463017a1
JasurJunior <zaripovjasurbek2000@mail.ru>
2021-12-02 19:52:35 +0500

    hammasini tugatdim :)

--- main.js
+++ main.js
@@ -21,26 +21,75 @@ const POGINATIONnum = document.querySelector('.title')
 //for categories
 const BTNS = document.querySelectorAll('.btns')
 
+//for search
+const GOsearch = document.querySelector('.btn')
+const INPsearch = document.querySelector('#search')
+const INPminSEARCH = document.querySelector('#min')
+const INPmaxSEARCH = document.querySelector('#max')
+const INPreytingSEARCH = document.querySelector('#score')
+
 //events======
+//event for search
+GOsearch.onclick = () => {
+  let filterDATA = [...NOWpages]
+
+  //search by name
+  if (INPsearch.value) {
+    filterDATA = filterDATA.filter(e => {
+      let { title } = e
+      return title.toLowerCase().includes(INPsearch.value.toLowerCase())
+    })
+    INPsearch.value = null
+  }
+
+  //search by years
+  if (INPminSEARCH.value) {
+    filterDATA = filterDATA.filter(e => {
+      let { release_date } = e
+      console.log(release_date.slice(0, 4), INPminSEARCH.value)
+      return release_date.slice(0, 4) >= INPminSEARCH.value
+    })
+    INPminSEARCH.value = null
+  }
+
+  if (INPmaxSEARCH.value) {
+    filterDATA = filterDATA.filter(e => {
+      let { release_date } = e
+      return release_date.slice(0, 4) <= INPmaxSEARCH.value
+    })
+    INPmaxSEARCH.value = null
+  }
+
+  //search by reyting
+  if (INPreytingSEARCH.value) {
+    filterDATA = filterDATA.filter(e => {
+      let { vote_average } = e
+      return vote_average >= INPreytingSEARCH.value
+    })
+    INPreytingSEARCH.value = null
+  }
+
+  //and render
+  renderFUN(filterDATA)
+}
+
+//event for categoris
 BTNS.forEach((e, i) => {
   e.onclick = () => {
+    _pages = 1
+    POGINATIONnum.innerHTML = _pages
     START(i)
   }
 })
 
-PREVbtn.onclick = () => {
-  if (_pages == 1) return
-  _pages--
-  START()
-}
-
+//event for paginations
 PREVbtn.onclick = () => {
   if (_pages == 1) return
   _pages--
   POGINATIONnum.innerHTML = _pages
   START()
 }
-
+//event for paginations
 NEXTbtn.onclick = () => {
   _pages++
   POGINATIONnum.innerHTML = _pages
